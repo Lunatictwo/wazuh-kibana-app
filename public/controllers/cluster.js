59fdbee7daed82a3677b1cf584a788ed0c6fec35
Jesús Ángel González <jesusgonzalez@wazuh.com>
2018-03-14 21:40:43 +0100

    Implements almost logic on cluster controller

--- public/controllers/cluster.js
+++ public/controllers/cluster.js
@@ -1,31 +1,110 @@
 const app = require('ui/modules').get('app/wazuh', []);
 
 // Logs controller
-app.controller('clusterController', function ($scope, $rootScope, errorHandler,apiReq) {
+app.controller('clusterController', function ($scope, $rootScope, errorHandler, apiReq,ClusterNodes,$window,$location) {
+    $scope.loading    = true;
     $scope.showConfig = false;
     $scope.showNodes  = false;
-    $scope.nodes = {items:[{
-        "node": "grulla_node",
-        "status": "connected",
-        "url": "192.168.0.134",
-        "cluster": "cluster01",
-        "type": "master",
-        "localhost": true
-        }]}
+    $scope.showFiles  = false;
+    $scope.nodeSearchTerm = '';
+    $location.search('tab','general')
+    
+    $scope.nodes      = ClusterNodes;
+
+    $rootScope.currentImplicitFilter = ""
+    $rootScope.ownHandlers = [];
+    $rootScope.loadedVisualizations = [];
+    $rootScope.tabVisualizations = {
+        general   : 1
+    };
+
+    const cleanHandlers = () => {
+        if ($rootScope.ownHandlers) {
+            for (let h of $rootScope.ownHandlers) {
+                h._scope.$destroy();
+            }
+        }
+
+        $rootScope.ownHandlers = [];
+    }
+
+    const setBooleans = component => {
+        $scope.showConfig = component === 'showConfig';
+        $scope.showNodes  = component === 'showNodes';
+        $scope.showFiles  = component === 'showFiles';
+    }
+
     $scope.goAgents = () => {
-        console.log('Go agents!');
+        cleanHandlers();
+        $window.location.href = '#/agents-preview';
     }
 
     $scope.goConfiguration = () => {
-        $scope.showConfig = true;
+        cleanHandlers();
+        setBooleans('showConfig');
     }
 
     $scope.goNodes = () => {
-        $scope.showNodes = true;
+        cleanHandlers();
+        setBooleans('showNodes');
+    }
+
+    $scope.goFiles = () => {
+        cleanHandlers();
+        setBooleans('showFiles');
     }
 
     $scope.goBack = () => {
-        $scope.showConfig = false;
-        $scope.showNodes  = false;
+        cleanHandlers();
+        setBooleans(null);
     }
+
+    const load = async () => {
+        try {
+            await $scope.nodes.nextPage();
+            
+            const data = await Promise.all([
+                apiReq.request('GET','/cluster/files',{}),
+                apiReq.request('GET','/cluster/agents',{}),
+                apiReq.request('GET','/cluster/status',{}),
+                apiReq.request('GET','/cluster/nodes',{}),
+                apiReq.request('GET','/manager/configuration',{section:'cluster'}),
+                apiReq.request('GET','/version',{})
+            ]);
+
+            const files = data[0]
+            const firstKey =  Object.keys(files.data.data)[0];
+            $scope.filesCount = 0;
+            for(let key in firstKey) $scope.filesCount += firstKey[key].length;
+
+            const agents = data[1]
+            $scope.agentsCount = 0;
+            for(let key in agents.data.data) $scope.agentsCount += agents.data.data[key].length;
+            
+            const status = data[2]
+            $scope.status = status.data.data.running;
+            
+            const nodesCount = data[3]
+            $scope.nodesCount = nodesCount.data.data.totalItems;
+
+            const configuration = data[4]
+            $scope.configuration = configuration.data.data;
+            
+
+            const version = data[5]
+            $scope.version = version.data.data;
+
+            $scope.loading = false;
+            if(!$scope.$$phase) $scope.$digest();
+            return;
+        } catch(error) {
+            errorHandler.handle(error,'Cluster')
+        }
+    }
+
+    load();
+
+    $scope.$on('destroy',() => {
+        cleanHandlers();
+    })
 });
